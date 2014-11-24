2f170e28e864bd0ba20ff77b853821e74e168392
Tyler Travis <battlemanner@gmail.com>
2014-11-24 11:34:11 -0700

    Added problem 3 to tex file and added more comments to part2.cpp

--- src/part2.cpp
+++ src/part2.cpp
@@ -67,8 +67,8 @@ int main(int argc, char** argv)
     x_dat << x[n].re << std::endl;
   }
 
-  //const int nfft = 1024;
-  
+  // Cacluating the fft using the overlap and save method
+  // using the fft842 with a 1024-point fft
   int M = 256;
   int overlap = M-1;
   int nfft = 1024; 
@@ -76,36 +76,48 @@ int main(int argc, char** argv)
 
   complx H[nfft];
   memcpy(H, h, sizeof(h));
-  // generate fft
+  // generate fft of the filter
   fft842(0, nfft, H);
+  
+  // Send the data to the corrisponding file
   for(int i = 0; i < nfft; ++i)
   {
     H_dat << H[i].re << "\t" << H[i].im <<std::endl;
   }
 
+  // yt is a temp variable for y - the output
   complx yt[nfft];
+  // xt is a temp variable for storing the correct values of x
+  // for computing the fft and multiplying it by the filter's resposne
   complx xt[nfft];
 
+  // The process for the computing the convolution
   int position = 0;
   while(position + nfft <= N)
   {
+    // Pull out the required data of the x input
     for(int j = 0; j < nfft; ++j)
     {
       xt[j] = x[j + position];
     }
 
+    // Calculating the corrisponding fft
     fft842(0, nfft, xt);
+    // Multiply the points of the x and h magnitude
     for(int k = 0; k < nfft; ++k)
     {
       yt[k] = mult(xt[k], H[k]);
     }
+    // Compute the inverse fft
     fft842(1, nfft, yt);
+    // The overlap-save portion
     for(int j = M-1; j < nfft; ++j)
     {
       y[j-M+position] = yt[j];
     }
     position += stepsize;
   }
+  // Output the data
   for(int n = 0; n < N; ++n)
   {
     y_dat << y[n].re << std::endl;
@@ -113,6 +125,7 @@ int main(int argc, char** argv)
   return 0;
 }
 
+// multiply complex data correctly
 complx mult(complx a, complx b)
 {
   complx ret;
